25e0468b0a29c76058359da93c35c1f78918e333
Gustavocode-web <gustavonunesbr1@gmail.com>
2026-01-16 14:16:26 -0300

    fiz alteracoes para conseguir puxar valores e quantidade de transacoes  por metodo de pagamento, e inseri filtros(starDate/endDate)

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -1,6 +1,7 @@
 import { Prisma } from "@prisma/client";
 import { Router } from "express";
 import { prisma } from "../lib/prisma";
+import { count } from "console";
 
 export const dashboardRoutes = Router();
 
@@ -9,22 +10,57 @@ dashboardRoutes.get("/", async (req, res) => {
     /* const totalTransactions = await prisma.transaction.count();
       return res.json({ totalTransactions }); */
 
-    const totalValue = await prisma.transaction.aggregate({
-        where: {
-            categoryId: 1 
+    const { startDate, endDate } = req.query;
+
+    const where: any = {
+        categoryId: 1,
+    };
+
+    if (typeof startDate === "string" || typeof endDate === "string") {
+        where.createdAt = {};
+
+        if (typeof startDate === "string") {
+            const start = new Date(`${startDate}T00:00:00.000`);
+            if (Number.isNaN(start.getTime())) {
+                return res.status(400).json({ error: "startDate inválida. Use YYYY-MM-DD" });
+            }
+            where.createdAt.gte = start;
+        }
+
+        if (typeof endDate === "string") {
+            const end = new Date(`${endDate}T23:59:59.999`);
+            if (Number.isNaN(end.getTime())) {
+                return res.status(400).json({ error: "endDate inválida. Use YYYY-MM-DD" });
+            }
+            where.createdAt.lte = end;
+        }
+    }
+
+
+    const countTransactions = await prisma.transaction.aggregate({
+        where,
+        _sum: {
+            value: true
         },
+        _count: { _all: true },
+    });
+
+    const total_e_soma_por_metodo_pagamento = await prisma.transaction.groupBy({
+        by: ['paymentMethod'],
+        where,
         _sum: {
             value: true,
         },
-    });
-
-    const countTransactions = await prisma.transaction.count({
-        where: {
-            categoryId: 1 
+        _count: {
+            _all: true,
         },
     });
 
-    return res.json({"O valor total de todas as transações do tipo vendas: ": totalValue, "Quantidade de transações do tipo vendas: ": countTransactions });
-    
+
+
+
+
+    return res.json({ "O valor total de todas as transações do tipo vendas: ": total_e_soma_por_metodo_pagamento, "Quantidade de transações do tipo vendas: ": countTransactions });
+
 
 });
